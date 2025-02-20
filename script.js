9935a6482b9451348fa81215e59441eed2559deb
zoyirr <zoyir6171@gmail.com>
2025-02-20 14:44:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// var a=20;
+// let ism="zoyirjon";
+// const yil=2025;
+// a=26
+// yil="2026"
+// console.log(yil);
+
+// confirm("davom etamz")
+
+let ism2=prompt("ismingizni kiriting")
+console.log("sizning ismingiz: "+ ism2);
+
+
+let ism3=prompt("familiyangizni kiriting")
+console.log("sizning familiyangiz: "+ ism3);
+
+
+let ism4=prompt("yoshingizni kiriting")
+console.log("sizning yoshimgiz: "+ ism4);
+
+let ism5=prompt("qaysi maktabda o'qiysiz")
+console.log("sizning yoshimgiz: "+ ism5);
+
+
+let ism6=prompt("yoqtirgan faningiz")
+console.log("yoqtirgan faningiz: "+ ism6);
+
+let ism7=prompt("yashaydigan tumaningizni kiriting")
+console.log("yoqtirgan faningiz: "+ ism7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+alert("malumotlar uchun rahmat!!!")
